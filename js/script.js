e428de0523187dda382abc09a7dd64c8d048b1df
anildalar <dollorinfotech@gmail.com>
2023-06-15 09:10:18 +0530

    new

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+//There are 2 thing related with functions
+
+
+//2. Function calling/invoking  call/invoke
+
+getmyname();
+getMySurname();
+
+//1. Function defination  is a one time process
+//Bad software enginner
+function getmyname(){ //  This is not a per the camelCase standar
+    console.log("Anil");
+}
+
+function getMySurname(){ // This function name is as per the camelCase standar
+    // Function body
+    //Inside the funciton body i can write many statements
+    console.log("DOLLOR"); // Statement = Instructions
+}
+
+  //2. Functin call
+  getMyAddress("Neemuch MP INDIA");
+    
+
+  //1. Function defination
+  function getMyAddress(adrr){
+     console.log(adrr)
+ }
---
+++ js/script2.js
@@ -0,0 +1,15 @@
+// Lets create a function which can add 2 numbers
+
+//1. Function defination (One time)
+
+function addTwoNumbers(n1,n2){
+
+    //every function return something
+    return n1 + n2;
+}
+
+//2. Function calling (Many Time)
+    //Associativity Priciple
+    // Left  = Right
+var result = addTwoNumbers(10,20);
+console.log(result);
